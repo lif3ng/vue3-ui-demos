bdcfbe94132c4a9156f36b9f128cabaee73cc677
lif3ng <lif3ng@icloud.com>
2023-04-09 15:40:34 +0800

    feat: auto-animate

---
+++ pages/animate/auto-animate.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+import { useAutoAnimate } from '@formkit/auto-animate/vue'
+
+const list = ref(['a', 'b', 'c'])
+const [parent] = useAutoAnimate()
+</script>
+
+<template>
+  <div ref="parent" class="flex-inline gap-2">
+    <div v-for="item, i in list" :key="item" class="w-5 h-5 flex items-center justify-center bg-blue " @click="list.splice(i, 1)">
+      {{ item }}
+    </div>
+  </div>
+  <div class="flex-inline gap-2">
+    <button class="px-4 bg-green" @click="list.sort()">
+      sort
+    </button>
+    <button class="px-4 bg-green" @click="list.sort().reverse()">
+      sort reverse
+    </button>
+  </div>
+</template>
