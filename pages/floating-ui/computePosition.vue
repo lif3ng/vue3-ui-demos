9120a3fc76e49621ab3ea47efc8d9480db02b8c6
lif3ng <lif3ng@icloud.com>
2023-04-30 21:47:54 +0800

    floating-ui

---
+++ pages/floating-ui/computePosition.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { computePosition } from '@floating-ui/dom'
+
+const reference = ref(null)
+const floating = ref(null)
+
+onMounted(() => {
+  computePosition(reference.value, floating.value, { placement: 'right' }).then(({ x, y }) => {
+    console.log({ x, y })
+    Object.assign(floating.value.style, {
+      left: `${x}px`,
+      top: `${y}px`,
+    })
+  })
+})
+</script>
+
+<template>
+  <button ref="reference" class="w-30 h-10 mt-20 mx-auto border">
+    reference
+  </button>
+  <div ref="floating" class="absolute border">
+    floating
+  </div>
+</template>
